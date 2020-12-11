9135e5bc11e16e24b7d25ba3bb9b612a3c3b0180
loic <loicmetringer@gmail.com>
2020-12-11 10:42:54 +0100

    darkmode en .js et le mode sombre qui marche dans cours

---
+++ public/js/darkmode.js
@@ -0,0 +1,36 @@
+//     function incrementValue() {
+//         var value = parseInt(document.getElementById('number').value, 0);
+//         value = isNaN(value) ? 0 : value;
+//         value++;
+//         document.getElementById('number').value = value;
+//
+//  if (value%2==1){
+//  window.onload=modeS()
+//  } else{
+//  }
+// }
+
+function modeS() {
+    var element = document.getElementById("containerID");
+    var element2 = document.getElementById("btnID");
+    var element3 = document.getElementById("btnID2");
+    var element4 = document.getElementById("btnID3");
+    var element5 = document.getElementById("btnID4");
+    var element6 = document.getElementById("btnID5");
+    var element7 = document.getElementById("btnID6");
+    var element8 = document.getElementById("btnID7");
+
+    element.classList.toggle("dark_mode");
+    element2.classList.toggle("backwhite");
+    element3.classList.toggle("backwhite");
+    element4.classList.toggle("backwhite");
+    element5.classList.toggle("backwhite");
+    element6.classList.toggle("backwhite");
+    element7.classList.toggle("backwhite");
+    element8.classList.toggle("backwhite");}
+
+
+
+
+
+
